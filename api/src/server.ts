8cd21d37db5c98124365a3600bc67577cb11bafc
Everton Silva <57024315+evertonos2312@users.noreply.github.com>
2021-02-24 12:48:13 -0300

    Dia 2

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,15 @@
-import express from 'express';
+import express, { request } from 'express';
 
 const app = express();
+
+app.get("/", (request, response)=>{
+
+    return response.json({message: "Hello World - NLW04"});
+});
+
+app.post("/", (request, response)=>{
+
+    return response.json({message: "Os dados foram salvos com sucesso!"});
+});
 
 app.listen(3333, () => console.log("Server is running"));
