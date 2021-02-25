37847c9b19e2b41d182a748887a67f4d43e4803a
Everton Silva <57024315+evertonos2312@users.noreply.github.com>
2021-02-25 20:58:42 -0300

    DIa 4
    Criado email service

---
+++ api/src/controllers/SendMailController.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysRepository } from "../respositories/SurveysRepository";
+import { SurveysUsersRepository } from "../respositories/SurveysUsersRepository";
+import { UsersRepository } from "../respositories/UsersRepository";
+import SendMailService from "../services/SendMailService";
+
+class SendMailController 
+{
+    async execute(request: Request, response: Response){
+        const { email, survey_id } = request.body;
+
+        const usersRepository = getCustomRepository(UsersRepository);
+        const surveysRepository = getCustomRepository(SurveysRepository);
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+        const userAlreadyExists = await usersRepository.findOne({email});
+
+        if(!userAlreadyExists){
+            return response.status(400).json({
+                error: "User does not exists",
+            });
+
+        }
+
+        const survey = await surveysRepository.findOne({id: survey_id});
+
+        if(!survey){
+            return response.status(400).json({
+                error: "Survey does not exists!"
+            })
+        }
+
+        // Salvar as informações na tabela surveyUser
+        const surveyUser = surveysUsersRepository.create({
+            user_id: userAlreadyExists.id,
+            survey_id
+        });
+
+
+        await surveysUsersRepository.save(surveyUser);
+        //Enviar e-mail para o usuário
+
+        await SendMailService.execute(email, survey.title, survey.description);
+
+        return response.json(surveyUser);
+
+
+
+    }
+}
+
+export { SendMailController }
